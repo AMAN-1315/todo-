8c912f4555ea769de68bce32dd2ea8b65f918d69
AMAN-1315 <amankumarcodes7@gmail.com>
2026-07-16 23:21:47 +0530

    Stage 4: full CRUD

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ class Tasks(BaseModel):
 
 
 my_tasks = [
-    {"id":1, "title": "Buy rasgulla", "completed": False},
-    {"id":2, "title": "Call Srinjoni", "completed": False},
-    {"id":3, "title": "build api", "completed": False},
+    {"id":1, "title": "Buy rasgulla", "done": False},
+    {"id":2, "title": "Call Srinjoni", "done": False},
+    {"id":3, "title": "build api", "done": False},
 ]
 
 
@@ -47,9 +47,34 @@ async def gettask(id:int,response:Response):
 async def createtask(task:Tasks):
     task_dict=task.model_dump()
     if (("title" in task_dict and task_dict['title'] is not None) and task_dict["title"] is not ""):
-        task_dict['id']=randrange(4,10)
+        task_dict['id']=randrange(4,100000000000)
         my_tasks.append(task_dict) 
         return task_dict
     else:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+        
+@app.put("/tasks/{id}")
+async def updatetask(id:int,task:Tasks):
+    taskdict = task.model_dump()
+    if (("title" in taskdict and taskdict['title'] is not None) and taskdict["title"] != ""):
+        for work in my_tasks:
+            if (work['id']==id):
+                work['title']=taskdict['title']
+                if "done" in taskdict:
+                    work['done']=taskdict['done']
+                return work
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    else:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+    
+@app.delete("/tasks/{id}",status_code=status.HTTP_204_NO_CONTENT)
+async def deletetask(id:int):
+    for idx,work in enumerate(my_tasks):
+        if work['id']==id:
+            my_tasks.pop(idx)
+            return 
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)    
+
+            
+
         
